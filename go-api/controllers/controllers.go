631fadb632672eb6d9d060d9e2ad13f4ff7d4027
saritob <muhammadali.hisar@gmail.com>
2022-11-17 21:50:18 +0500

    book create api implementation completed

--- go-api/controllers/controllers.go
+++ go-api/controllers/controllers.go
@@ -1,9 +1,81 @@
 package controllers
 
-import "net/http"
+import (
+	"database/sql"
+	"encoding/json"
+	"example/books-api/models"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
-func CreateBook(w http.ResponseWriter, r *http.Request)  {}
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+type response struct {
+	ID      int64  `json:"id,omitempty"`
+	Message string `json:"messge,omitempty"`
+}
+
+func createConnection() *sql.DB {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error Loading .env file")
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected successfully with pg")
+	return db
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	var book models.Book
+	err := json.NewDecoder(r.Body).Decode(&book)
+
+	if err != nil {
+		log.Fatalf("Unable to decode the request body. %v", err)
+	}
+
+	insertID := insertBook(book)
+
+	res := response{
+		ID:      insertID,
+		Message: "Book created successfully",
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
 func GetAllBooks(w http.ResponseWriter, r *http.Request) {}
 func GetBookById(w http.ResponseWriter, r *http.Request) {}
 func UpdateBook(w http.ResponseWriter, r *http.Request)  {}
 func DeleteBook(w http.ResponseWriter, r *http.Request)  {}
+
+func insertBook(book models.Book) int64 {
+	db := createConnection()
+
+	defer db.Close()
+
+	sqlStatement := "INSERT INTO books(name,price,publisher) VALUES($1,$2,$3) RETURNING id"
+	var id int64
+	err := db.QueryRow(sqlStatement, book.Name, book.Price, book.Publisher).Scan(&id)
+
+	if err != nil {
+		log.Fatalf("Unable to execute the query %v", err)
+	}
+
+	fmt.Printf("inserted a single record with id %v", id)
+
+	return id
+
+}
--- go-api/go.mod
+++ go-api/go.mod
@@ -4,5 +4,7 @@ go 1.19
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.4.0
+	github.com/lib/pq v1.10.7
 	github.com/rs/cors v1.8.2
 )
