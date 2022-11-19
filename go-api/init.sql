50ae20b72ffedc67e4b6064f7e21d565ed2bde63
saritob <muhammadali.hisar@gmail.com>
2022-11-19 18:14:18 +0500

    users table sql query added

--- go-api/init.sql
+++ go-api/init.sql
@@ -1,6 +1,17 @@
-CREATE TABLE books (
+CREATE TABLE users (
     id serial primary key,
     name text,
     price decimal,
     publisher text
 );
+
+CREATE TABLE books (
+    id serial primary key,
+    name text,
+    price decimal,
+    publisher text,
+    user_id serial,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+
